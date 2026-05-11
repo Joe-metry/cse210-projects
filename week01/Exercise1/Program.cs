7e2580140765c512ee7c15ab0cb3168fbb64e568
Joe-metry <joemetry150@gmail.com>
2026-05-11 13:08:47 +0100

    A program that asks the user for their first and last name, then prints it out

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        // Exercise 1: Write a program that asks the user for their first and last name, then prints out a greeting in the format "Your name is [last name], [first name] [last name]."
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
+
     }
 }
