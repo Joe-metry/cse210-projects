b7eef3468d75b7c1708bdc964fd1f084621749cc
Joe-metry <joemetry150@gmail.com>
2026-05-12 11:10:31 +0100

    Completed Exercise 1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,13 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        //Author : Ubi, Joseph Bassey
+        //Author : Ubi, Joseph Bassey.
         // Exercise 1: Write a program that asks the user for their first and last name, then prints out a greeting in the format "Your name is [last name], [first name] [last name]."
         Console.Write("What is your first name? ");
         string firstName = Console.ReadLine();
         Console.Write("What is your last name? ");
         string lastName = Console.ReadLine();
-        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
+        Console.WriteLine($"Your name is: {lastName}, {firstName} {lastName}.");
 
     }
 }
