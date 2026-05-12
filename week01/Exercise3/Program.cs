7bbd80126ab935dff28ed6bc4add6e7dbb2464a1
Joe-metry <joemetry150@gmail.com>
2026-05-12 11:56:13 +0100

    completed Exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string response;
+        do
+        {
+        int guessCount = 0;
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+        while (true)
+        {
+            Console.Write("What is the magic number? ");
+            string input = Console.ReadLine();
+            int inputToInt = int.Parse(input);
+            guessCount++;
+            if (inputToInt > number)
+            {
+                Console.WriteLine("Too high! Try again.");
+            }
+            else if (inputToInt < number)
+            {
+                Console.WriteLine("Too low! Try again.");
+            }
+            else
+            {
+                Console.WriteLine("Congratulations! You guessed the magic number!");
+                Console.WriteLine($"You made {guessCount} guesses.");
+                Console.Write("Do you want to play again? (yes/no) ");
+                response = Console.ReadLine().ToLower();
+                break;
+            }
+        }
+        } while (response != "no");
+    
     }
 }
